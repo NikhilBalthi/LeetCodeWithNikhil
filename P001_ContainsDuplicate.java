af8d3f6510c68c3c98dedeb037a2f68d24dbdb7b
NikhilBalthi <86175945+NikhilBalthi@users.noreply.github.com>
2025-11-20 12:27:23 +0530

    Implement containsDuplicate method using HashSet

---
+++ P001_ContainsDuplicate.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class P001_ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (!seen.add(num)) return true;
+        }
+        return false;
+    }
+}
